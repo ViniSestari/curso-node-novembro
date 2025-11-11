ad071002a2a589facac244d739d8bf78540f54db
ViniSestari <vinifujisestari@gmail.com>
2025-11-11 19:56:53 -0300

    .

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log ("olá");
+const express = require('express');
+const app = express();
+app.get('/dc', function (req, res) {
+    const herois = ['Mulher Maravilha', 'Batman', 'Superman']
+    res.send(herois)
+})
+
+app.get('/marvel', function (req, res) {
+    const herois = ['Homem Aranha', 'Capitã Marvel', 'Capitão América']
+    res.send(herois)
+})
+
+app.listen(3000)
